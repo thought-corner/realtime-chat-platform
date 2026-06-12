109a47a01f3e4929a88f6e7f8e93ccf876fb584b
woo jin <dnwls16071@naver.com>
2026-06-13 02:33:13 +0900

    initialize project structure with Gradle, Spring Boot, and Kotlin support

---
+++ src/main/kotlin/com/project/realtime_chat_platform/RealtimeChatPlatformApplication.kt
@@ -0,0 +1,11 @@
+package com.project.realtime_chat_platform
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RealtimeChatPlatformApplication
+
+fun main(args: Array<String>) {
+	runApplication<RealtimeChatPlatformApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/realtime_chat_platform/RealtimeChatPlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.realtime_chat_platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RealtimeChatPlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
